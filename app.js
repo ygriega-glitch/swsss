2e21be609a2d17a83705d6e96512dc3f992791d8
ygriega-glitch <ygriegasoportetecnico@gmail.com>
2026-04-06 19:25:40 +0200

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-// ==============================
-// SUPABASE CONFIG
-// ==============================
-
 const SUPABASE_URL = "https://npukjptnqdlvwkejobby.supabase.co";
 const SUPABASE_KEY = "sb_publishable_4Gq4e044KeJvhdrdXHCKQw_gWQrAs30";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// ==============================
+// ===================
+// NAVIGATION
+// ===================
+
+function showSection(id) {
+  document.querySelectorAll(".sectionPage").forEach(sec => {
+    sec.style.display = "none";
+  });
+
+  document.getElementById(id).style.display = "block";
+}
+
+// default load
+showSection("dashboard");
+
+// ===================
 // CREATE CAMPAIGN
-// ==============================
+// ===================
 
 async function createCampaign() {
+  console.log("CLICK DETECTED");
 
   const name = document.getElementById("campaignName").value;
   const reviewLink = document.getElementById("reviewLink").value;
@@ -39,8 +51,7 @@ async function createCampaign() {
 
   const campaign = data[0];
 
-  // GENERATE LINK
   const link = window.location.origin + "/play.html?campaign=" + campaign.id;
 
-  alert("Campaign created!\n\nYour link:\n" + link);
+  alert("Campaign created!\n\n" + link);
 }
